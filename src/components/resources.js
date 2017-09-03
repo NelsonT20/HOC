08cdb893f5af4eec1fc7dd3cbb12d35078b49181
nelson torres <nelsondaniel88@gmail.com>
2017-09-03 17:25:03 -0400

    save changes

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default() =>{
+    return(
+        <div>
+            Super Special Secret Recipe
+            <ul>
+                <li>1 Cup sugar</li>
+                <li>1 Cup salt</li>
+                <li>1 Cup of milk</li>
+            </ul>
+        </div>
+    );
+} 
